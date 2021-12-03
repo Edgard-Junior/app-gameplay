9e15318d63c5dd31a96d18990b2b11a8e16cb86f
Edgard-Junior <edgard.juniormoraes@hotmail.com>
2021-12-02 22:12:48 -0300

    en-vsn

--- src/screen/SignIn/style.ts
+++ src/screen/SignIn/style.ts
@@ -1,14 +1,31 @@
 import { StyleSheet } from "react-native";
+import { theme } from "../../global/styles/themes";
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: theme.color.background
     },
-    input: {
-        height: 50,
-        width: 50,
-        borderBottomWidth: 2,
-    }
+    Image: {
+        width: '100%',
+        height: '360',
+    },
+    content: {
+        marginTop: -40,
+        
+    },
+    title: {
+        color: theme.color.heading,
+        textAlign: 'center',
+        fontSize: '40',
+        marginBottom: '16',
+    },
+    subtitle: {
+        color: theme.color.heading,
+        fontSize: 15,
+        textAlign: 'center';
+        marginBotton: 64,
+    },
 });
